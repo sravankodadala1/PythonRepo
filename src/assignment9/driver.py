15f6f489d312f76f77f044e1b6c168a87f63d468
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 15:50:46 +0530

    named_tuple

--- src/assignment9/driver.py
+++ src/assignment9/driver.py
@@ -1,11 +1,12 @@
 from util import class_avg_marks
 
-
+#enter the number of students
 num_students = int(input("enter number of students"))
+#enter the number of fields 
 columns = input("enter total fields").split()
-
-student_data = [input().split() for i in range(num_students)]
-
+#input the student fields
+student_data = [input("enter the student fields").split() for i in range(num_students)]
+#calling the function
 avg_marks = class_avg_marks(num_students, columns, student_data)
 print('{:.2f}'.format(avg_marks))
  
