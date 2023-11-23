ddceef9f9a2d381dd9453317b4747da77b9a3e44
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 19:59:06 +0530

    This is a util file for Text-Allignment program

--- src/assignment6/util.py
+++ src/assignment6/util.py
@@ -1,7 +1,7 @@
 def print_pattern(thickness, c='H'):
     result = []
  
-    # Top Cone
+    # Top Cone 
     for i in range(thickness):
         line = (c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1)
         result.append(line)
