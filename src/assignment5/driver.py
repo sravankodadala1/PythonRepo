ff65ad24f23771c5d3bee8d9f9aba4da0e207947
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 14:37:54 +0530

    merge the tools

--- src/assignment5/driver.py
+++ src/assignment5/driver.py
@@ -2,7 +2,7 @@ from util import updated_string
 
 #taking input of string from the user
 string = input("Enter the string")
-#taking value of k from the user
+#taking value of k from the user 
 k = int(input("Enter k value "))
 
 #calling the function
