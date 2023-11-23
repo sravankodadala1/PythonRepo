4e98ced98270b20a610377698f02b096c057fe93
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 20:00:56 +0530

    This is a util file for second-maximum-number-in-the list program

--- src/assignment3/util.py
+++ src/assignment3/util.py
@@ -1,5 +1,5 @@
 def Runner_up_score(n, arr):
     #conver the arr into string and sorted
     runner_up = sorted(set(arr))
-    #return the second last value
+    #return the second last value 
     return runner_up[-2]
