8468867e430bab260eefb5f84f9dee607b372f98
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 19:40:50 +0530

    This is a util file for python-mutations program

--- src/assignment4/util.py
+++ src/assignment4/util.py
@@ -1,5 +1,5 @@
 def mutate_string(string, position, character):
-    #converting the values into list
+    #converting the values into list 
     a=list(string)
     #assigning character at given position index
     a[position]=character
