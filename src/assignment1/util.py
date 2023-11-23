1fc3b14534f5d77bdf090c1ea4b50521d0c61103
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 18:48:25 +0530

    This is a util file for find the percentage program

--- src/assignment1/util.py
+++ src/assignment1/util.py
@@ -1,6 +1,6 @@
 from util import list_operations
 from src.assignment1.util import list_operations
-
+ 
 if __name__ == '__main__':
     N = int(input("Enter the number of commands: "))
     user_commands = [input().split() for i in range(N)]
