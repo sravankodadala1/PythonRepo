37e2e5ff8043a6661d8ad67845fdb57ac8daf831
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 16:14:58 +0530

    time delta

--- src/assignment10/driver.py
+++ src/assignment10/driver.py
@@ -1,13 +1,13 @@
 from util import calculate_time_delta
 import os
 
- 
+#input the number of test cases 
 t = int(input("Enter the number of test cases: "))
-
+#taking input of t1 and t2
 for i in range(t):
     t1 = input("Enter the first time: ")
     t2 = input("Enter the second time: ")
-
+    #calling the function
     delta = calculate_time_delta(t1, t2)
 
     print(delta)
