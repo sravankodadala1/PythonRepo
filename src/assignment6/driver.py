84310604f6910995099a4e2e4c718c1f3fdab827
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 14:38:53 +0530

    string_formatting

--- src/assignment6/driver.py
+++ src/assignment6/driver.py
@@ -3,6 +3,6 @@ from util import print_pattern
 
 # Take user input for the thickness
 thickness = int(input("Enter thickness (must be an odd number): "))
-
+ 
 # Call the utility function to generate and print the pattern
 print_pattern(thickness)
