9c21f66da03cc14c963dc4901ce859cc3b5eb710
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 15:58:24 +0530

    namedtuple

--- src/assignment9/util.py
+++ src/assignment9/util.py
@@ -1,11 +1,15 @@
 from collections import namedtuple
 
 def class_avg_marks(num_students, columns, student_data):
+    
     total = 0
+    #creating a namedtuple
     Student = namedtuple('Student', columns)
-
+    
     for i in student_data:
+        #assigning each element of list to corresponding field of named tuple
         student = Student(*i)
+        #adding the marks of students
         total += int(student.marks)
-
+    #return average marks of students
     return total / num_students
