660a0382f728a1d8edfdc5c53e2af0e7b5c74ea6
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-23 19:33:16 +0530

    This is a util file for Pilling up program

--- src/assignment17/util.py
+++ src/assignment17/util.py
@@ -2,5 +2,5 @@ def Pilling_up(lst):
     min_index = lst.index(min(lst))
     left = lst[:min_index]
     right = lst[min_index + 1:]
-
+ 
     return left == sorted(left, reverse=True) and right == sorted(right)
