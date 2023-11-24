6cf1a57c636a5b65a5a6d5f7f3eddce709ed980d
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-24 11:27:01 +0530

    This is a util program for find-the-percentage program

--- src/assignment2/util.py
+++ src/assignment2/util.py
@@ -1,5 +1,5 @@
 def total_marks(d, student_name):
-    for name, scores in d.items():
+    for name, scores in d.items(): 
         if student_name == name:
             percentage = sum(scores) / len(scores)
             print("The average percentage for {} is: {:.2f}%".format(name, percentage))
