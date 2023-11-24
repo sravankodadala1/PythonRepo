28f3aca1713fded73753eae30261d1ff79cff357
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-24 11:32:56 +0530

    This is a test file for no idea program

--- test/assignment14/test.py
+++ test/assignment14/test.py
@@ -2,7 +2,7 @@ import unittest
 from src.assignment14.util import happiness
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_something(self): 
         array_values = [2,3,4,5,6,7,8]
         a = {3,1,1}
         b = {3,1,10}
