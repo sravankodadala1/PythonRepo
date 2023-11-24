857ee57573f36306d9717ccd182e43c07214b31c
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-24 11:29:49 +0530

    This is a test file for time delta program

--- test/assignment10/test.py
+++ test/assignment10/test.py
@@ -3,7 +3,7 @@ from src.assignment10.util import calculate_time_delta
 
 class MyTestCase(unittest.TestCase): 
     def test_something(self):
-        t1 = "Sun 10 May 2015 13:54:36 -0700"
+        t1 = "Sun 10 May 2015 13:54:36 -0700" 
         t2 = "Sun 10 May 2015 13:54:36 -0000"
 
         result = calculate_time_delta(t1, t2)
