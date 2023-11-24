9c1f14a7e747f3235913e62c3e2a54a5e9cb428f
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-24 11:25:57 +0530

    This is a util file for floor-ceil-rint program

--- src/assignment11/util.py
+++ src/assignment11/util.py
@@ -2,7 +2,7 @@ import numpy
 
 
 def process_array(input_array):
-    numpy.set_printoptions(sign=' ')
+    numpy.set_printoptions(sign=' ') 
     floor_result = numpy.floor(input_array)
     ceil_result = numpy.ceil(input_array)
     rint_result = numpy.rint(input_array)
